d679bc3091190650eff21024617ff50b7e6862d9
LinYuChengJason <a0936658981@gmail.com>
2019-05-30 00:43:53 +0800

    no message

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get('/broadcast', function(request, response){
 
 function find(collection, query, callback){
 	let collectionTarget = myDB.collection(collection);
-	collectionTarget.find({"JS1902-01" : { $exists: true }}).project({"JS1902-01" : 1}).toArray(function(err, docs){
+	collectionTarget.find({"JS1902-01" : { $exists: true }}).project({"_id" : 0}).toArray(function(err, docs){
 		callback(err, docs);
 	});
 }
