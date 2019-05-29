f48cd1a9990c76b4e526e86b829212e8d29e34f2
LinYuChengJason <a0936658981@gmail.com>
2019-05-30 01:28:34 +0800

    no message

--- index.js
+++ index.js
@@ -87,8 +87,10 @@ app.get('/broadcast', function(request, response){
 })
 
 function find(collection, query, callback){
+	let jobj = JSON.parse(JSON.stringify("{_id:0,JS1902-01:1}"));
+
 	let collectionTarget = myDB.collection(collection);
-	collectionTarget.find({}).project(query).toArray(function(err, docs){
+	collectionTarget.find({}).project(jobj).toArray(function(err, docs){
 		callback(err, docs);
 	});
 }
